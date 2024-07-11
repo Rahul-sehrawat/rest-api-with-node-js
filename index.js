3fc1d7e5d9e9e4d0762f2e3af4d17931152ada81
Rahul sehrawat <rahulsehrawat1205@gmail.com>
2024-07-11 12:20:13 +0530

    custom middleware

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const PORT = 3000
 app.use(express.urlencoded({extended:false})) // this middleware is for encoded form request
 app.use(express.json()) // this middleware is for json request
 
+//custom middleware
+app.use((req,res,next)=>{
+    fs.appendFile("log.txt",`\n ${Date.now()}: ${req.method}: ${req.path}`,(err,data)=>{
+        next()
+    })
+})
+
 app.get("/",(req,res)=>{
     res.end(" hello rahul !!!")
 })
